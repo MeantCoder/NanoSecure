41a5e2e50c7a326fef71f0ca4df6e47b188fe066
Ugur D <meantcoder@gmail.com>
2015-09-29 18:38:13 +0300

    Kararlı Sürüm [29.09.2015]
    + Çekirdek ve Framework yapısı birleştirildi.
    + I/O Kullanımı eklendi.
    + net.* Modül yapısına bağlı kalınarak ufak düzeltmeler yapıldı.
    + Server ve Client tek bir çekirdek yapısı altında toplandı.
    + Framework kütüphane şekline getirildi.
    * Kararlı Sürüm - Alpha 1

--- Stabil Sürüm -- Framework/framework.lua
+++ Stabil Sürüm -- Framework/framework.lua
@@ -2,25 +2,18 @@
 -- Relesease Date : 29.09.2015
 -- Compatible with net.Server
 -- 7716 (Firmware)
+
+
+-- Framework info log --
 framework={}
     framework.version = "ALPHA 1"
-    framework.compatible = "net.Server, LUA & NodeMCU Firmware Devices"
+    framework.compatible = "net.*, LUA & NodeMCU Firmware Devices"
     framework.users = "For Developers"
-<<<<<<< HEAD
 
 -------------------------------------------------
 
-var server_name = server -- Sunucu Deðiþkeni
-var client_name = client -- Ýstemci Deðiþkeni
-var net.type = net.TCP -- Ýstemci Baðlantý Tipi
-int net.timeout = 0 -- Ýstemci Zaman Aþýmý
-int net.Port = 1905 -- Sunucu Portu
-var net.Server = "localhost" -- Sunucu Adresi
-var sensor_data = 0 -- Sensör Varsayýlan Deðer
-=======
-    
--------------------------------------------------
 
+-- DeÄŸiÅŸkenler --
 var server_name = server -- Sunucu DeÄŸiÅŸkeni
 var client_name = client -- Ä°stemci DeÄŸiÅŸkeni
 var net.type = net.TCP -- Ä°stemci BaÄŸlantÄ± Tipi
@@ -28,22 +21,23 @@ int net.timeout = 0 -- Ä°stemci Zaman AÅŸÄ±mÄ±
 int net.Port = 1905 -- Sunucu Portu
 var net.Server = "localhost" -- Sunucu Adresi
 var sensor_data = 0 -- SensÃ¶r VarsayÄ±lan DeÄŸer
->>>>>>> 85da132016d65bfcb51d1824aaf543279d9986be
+-------------------------------------------------
 
 
 -- Client --
 var client_name.on = client_name:on
 var client_name.send = client_name:send
 var client_name.connect = client_name:connect
+var client_name.close = client_name:close
 var client_name.createConnection = client_name:createConnection
 -------------------------------------------------
 
 -- Server --
 var server_name.on = server_name:on
-var server_name.send = server_name.send
+var server_name.send = server_name:send
 var server_name.listen = server_name:listen
-var server_name.close = server_name.close
-var server_name.createServer = server_name.createServer
+var server_name.close = server_name:close
+var server_name.createServer = server_name:createServer
 -------------------------------------------------
 
 -- Framework bilgisi --
@@ -68,14 +62,9 @@ var io.trig = gpio.trig
 if ((net.Port > 28800) or (net.Port < 1))
     server_name.close
     client_name.close
-<<<<<<< HEAD
-    print("Port 1'den küçük 28800 'den büyük olamaz!")
-    print("\r\n Þuanki Port :: " + net.Port)
 
--------------------------------------------------
-=======
     print("Port 1'den kÃ¼Ã§Ã¼k 28800 'den bÃ¼yÃ¼k olamaz!")
     print("\r\n Åžuanki Port :: " + net.Port)
 
 -------------------------------------------------
->>>>>>> 85da132016d65bfcb51d1824aaf543279d9986be
+
